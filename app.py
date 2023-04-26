d8fc47471f3a2c84919df0bbc7b01becc82b45b2
edhumazzoni <eduardo.rmazzoni@gmail.com>
2023-04-26 00:30:44 -0300

    Lista em com retorno json 1.2

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    jogos = {
+    jogos = [
+        {
     'Playstation': ['Castlevania', 'Final Fantasy Tactics'],
     'SNES': ['Super Metroid', 'Super Mario World'],
     'Saturn': ['Nights', 'Guardian Heroes'],
     'Xbox': ['Halo', 'Gears of War'],
 }
+    ]
     return (jogos)
 if __name__ == "__main__":
     app.run()
